1f3da446cf97ab5eaf57b132062e1b540274ad46
briansachaa <briansachaasang@gmail.com>
2019-04-07 11:34:57 +0300

    added global variables and construction function for player

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var player1, player2;
+//Back-End Logic
+//Constructor function for a player
+function Player(name, turnTotal, diceRoll, overallScore, active) {
+    this.name = name;
+    this.diceRoll = 0;
+    this.turnTotal = 0;
+    this.overallScore = 0;
+    this.active = active;
