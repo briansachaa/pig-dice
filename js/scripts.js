a3bb2a8230dd3300f1fd6eb0609121be24c144c2
briansachaa <briansachaasang@gmail.com>
2019-04-07 11:42:07 +0300

    added function to enable and disable the gamers according to which player is active

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//global variables
 var player1, player2;
 //Back-End Logic
 //Constructor function for a player
@@ -7,3 +8,18 @@ function Player(name, turnTotal, diceRoll, overallScore, active) {
     this.turnTotal = 0;
     this.overallScore = 0;
     this.active = active;
+}
+//Function to disable and enable gaming areas according to which player is active.
+function activeUser() {
+    if (player1.active === true && player2.active === false) {
+        $('.player1Area').children().prop('disabled', false);
+        $('.player1Area').removeClass('disableGamingArea');
+        $('.player2Area').children().prop('disabled', true);
+        $('.player2Area').addClass('disableGamingArea');
+    } else {
+        $('.player1Area').children().prop('disabled', true);
+        $('.player1Area').addClass('disableGamingArea');
+        $('.player2Area').children().prop('disabled', false);
+        $('.player2Area').removeClass('disableGamingArea');
+    }
+};
